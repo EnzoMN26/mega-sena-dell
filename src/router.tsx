fb190c95407299a68b996a326f810ebe46531a41
EnzoMN26 <enzodrao@gmail.com>
2024-03-15 17:29:22 -0300

    ajeitando o router

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
+import Registro from "./components/Registro";
+import Sorteio from "./components/Sorteio";
+import Loja from "./components/Loja";
+import Inventario from "./components/Inventario";
+import Aposta from "./components/Aposta";
 
 const router = createBrowserRouter([
   {
@@ -7,8 +12,25 @@ const router = createBrowserRouter([
     element: <Home />,
     children: [
       {
-        path: "/registro"
-      }
+        path: "/registro",
+        element: <Registro />,
+      },
+      {
+        path: "/Sorteio",
+        element: <Sorteio />,
+      },
+      {
+        path: "/Loja",
+        element: <Loja />,
+      },
+      {
+        path: "/Inventario",
+        element: <Inventario />,
+      },
+      {
+        path: "/Aposta",
+        element: <Aposta />,
+      },
     ],
   },
 ]);
