aedcf7151bb5e35299a7dca3ae4b2178231c45e5
EnzoMN26 <enzodrao@gmail.com>
2024-03-15 23:59:08 -0300

    inicio da tela de sorteio, criacao da grade de numeros para selecionar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,12 +5,12 @@ const Menu: React.FC = () => {
     return( 
         <div id={styles.menu}>
             <div id={styles.menuLinks}>
-            <Link to="registro">Fazer Aposta</Link>
-            <Link to="loja">Loja</Link>
-            <Link to="inventario">Inventário</Link>
+            <Link className={styles.link} to="registro">Fazer Aposta</Link>
+            <Link className={styles.link} to="loja">Loja</Link>
+            <Link className={styles.link} to="inventario">Inventário</Link>
             </div>
             <div id={styles.footer}>
-                <Link to="sorteio">Realizar Sorteio</Link>
+                <Link className={styles.link} to="sorteio">Realizar Sorteio</Link>
             </div>
         </div>
     )
--- src/components/Sorteio.tsx
+++ src/components/Sorteio.tsx
@@ -2,7 +2,61 @@ import styles from "../styles/Sorteio.module.css"
 
 const Sorteio: React.FC = () => {
     return( 
-        <></>
+        <div id={styles.sorteio}>
+            <div className={styles.gridContainer}>
+                <button className={styles.gridItem}>1</button>
+                <button className={styles.gridItem}>2</button>
+                <button className={styles.gridItem}>3</button>
+                <button className={styles.gridItem}>4</button>
+                <button className={styles.gridItem}>5</button>
+                <button className={styles.gridItem}>6</button>
+                <button className={styles.gridItem}>7</button>
+                <button className={styles.gridItem}>8</button>
+                <button className={styles.gridItem}>9</button>
+                <button className={styles.gridItem}>10</button>
+                <button className={styles.gridItem}>11</button>
+                <button className={styles.gridItem}>12</button>
+                <button className={styles.gridItem}>13</button>
+                <button className={styles.gridItem}>14</button>
+                <button className={styles.gridItem}>15</button>
+                <button className={styles.gridItem}>16</button>
+                <button className={styles.gridItem}>17</button>
+                <button className={styles.gridItem}>18</button>
+                <button className={styles.gridItem}>19</button>
+                <button className={styles.gridItem}>20</button>
+                <button className={styles.gridItem}>21</button>
+                <button className={styles.gridItem}>22</button>
+                <button className={styles.gridItem}>23</button>
+                <button className={styles.gridItem}>24</button>
+                <button className={styles.gridItem}>25</button>
+                <button className={styles.gridItem}>26</button>
+                <button className={styles.gridItem}>27</button>
+                <button className={styles.gridItem}>28</button>
+                <button className={styles.gridItem}>29</button>
+                <button className={styles.gridItem}>30</button>
+                <button className={styles.gridItem}>31</button>
+                <button className={styles.gridItem}>32</button>
+                <button className={styles.gridItem}>33</button>
+                <button className={styles.gridItem}>34</button>
+                <button className={styles.gridItem}>35</button>
+                <button className={styles.gridItem}>36</button>
+                <button className={styles.gridItem}>37</button>
+                <button className={styles.gridItem}>38</button>
+                <button className={styles.gridItem}>39</button>
+                <button className={styles.gridItem}>40</button>
+                <button className={styles.gridItem}>41</button>
+                <button className={styles.gridItem}>42</button>
+                <button className={styles.gridItem}>43</button>
+                <button className={styles.gridItem}>44</button>
+                <button className={styles.gridItem}>45</button>
+                <button className={styles.gridItem}>46</button>
+                <button className={styles.gridItem}>47</button>
+                <button className={styles.gridItem}>48</button>
+                <button className={styles.gridItem}>49</button>
+                <button className={styles.gridItem}>50</button>
+            </div>
+            <div></div>
+        </div>
     )
 }
 
